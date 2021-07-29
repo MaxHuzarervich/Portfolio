4fdac3e7292c1414bc0309dd95d4202b9eee8b1a
MaxHuzarevich <pinsk.maks@mail.ru>
2021-07-29 20:22:01 +0300

    all ok

--- src/myWorks/MyWorks.js
+++ src/myWorks/MyWorks.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import style from './MyWorks.module.css'
+import style from './../common/styles/Block.module.css'
 import Work from "./work/Work";
 import styleContainer from "../common/styles/Container.module.css";
 
 function MyWorks() {
     return (
-        <div className={style.worksBlock}>
-            <div className={`${styleContainer.container} ${style.worksContainer}`}>
+        <div className={style.bigBlock}>
+            <div className={`${styleContainer.container} ${style.container}`}>
                 <h2 className={style.title}>Мои работы</h2>
-                <div className={style.works}>
+                <div className={style.littleBlock}>
                     <Work title={'Todo list'} description={' Описание Описание Описание Описание Описание Описание Описание Описание Описание Описание Описание Описание Описание Описание Описание Описание '}/>
                     <Work title={'Social Network'} description={'Описание Описание Описание Описание Описание Описание Описание Описание Описание Описание Описание Описание '}/>
                 </div>
--- src/skills/Skills.js
+++ src/skills/Skills.js
@@ -1,15 +1,15 @@
 import React from "react";
-import style from './Skills.module.css'
+import style from './../common/styles/Block.module.css'
 import styleContainer from './../common/styles/Container.module.css'
 import Skill from "./skil/Skill";
 
 
 function Skills() {
     return (
-        <div className={style.skillsBlock}>
-            <div className={`${styleContainer.container} ${style.skillsContainer}`}>
+        <div className={style.bigBlock}>
+            <div className={`${styleContainer.container} ${style.container}`}>
                 <h2 className={style.title}>Мои скиллы</h2>
-                <div className={style.skills}>
+                <div className={style.littleBlock}>
                     <Skill title={'JS'} description={' Описание Описание Описание    Описание Описание Описание Описание Описание Описание Описание Описание Описание Описание Описание Описание Описание Описание Описание Описание Описание Описание Описание Описание Описание Описание'}/>
                     <Skill title={'CSS'} description={'Описание Описание Описание Описание Описание Описание Описание Описание Описание Описание'}/>
                     <Skill title={'React'} description={' Описание Описание Описание Описание Описание Описание Описание Описание Описание Описание Описание Описание Описание Описание Описание Описание Описание Описание Описание моих навыков находится именно здесь!)'}/>
