b0a4961ebd290b1936a032014a874235cb1ccc0f
MaxHuzarevich <pinsk.maks@mail.ru>
2021-10-12 23:02:36 +0300

    2 part sass ok

--- src/skills/Skills.js
+++ src/skills/Skills.js
@@ -1,5 +1,5 @@
 import React from "react";
-import style from '../common/styles/Block.module.scss'
+import style from './Skills.module.css'
 import styleContainer from './../common/styles/Container.module.css'
 import Skill from "./skil/Skill";
 import Title from "../common/components/title/Title";
